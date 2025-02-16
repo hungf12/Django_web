0e7bdde2dbee85da2273644bdea9ddc841192cda
hungf12 <93297529+hungf12@users.noreply.github.com>
2025-02-16 15:23:31 +0700

    #17 full

--- app/models.py
+++ app/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+# change forms register django
+class CreateUserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username','email','first_name','last_name','password1','password2']
 # Create your models here.
 class Customer(models.Model):
     user = models.OneToOneField(User, on_delete=models.SET_NULL, null=True, blank=False)
