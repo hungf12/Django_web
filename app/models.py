a6d3c754c178374be0ebae5a72b9c06248cf4821
hungf12 <duyhung12032000@gmail.com>
2025-03-07 11:07:37 +0700

    07/03

--- app/models.py
+++ app/models.py
@@ -13,11 +13,13 @@ class CreateUserForm(UserCreationForm):
 class Product(models.Model):
     name = models.CharField(max_length=256, null=True)
     attribute = models.CharField(max_length=256,null=True)
-    price = models.FloatField()
+    # price = models.FloatField()
+    price = models.DecimalField(max_digits=10,decimal_places=0)
+    old_price = models.DecimalField(max_digits=10, decimal_places=0,null=True,blank=True)
     digital = models.BooleanField(default=False, null=True, blank=False)
     image = models.ImageField(null=True, blank=True)
-    storage_options = models.JSONField(default=list)
-    colors = models.JSONField(default=list)
+    storage_options = models.JSONField(default=dict)
+    colors = models.JSONField(default=dict)
     def __str__(self):
         return self.name
     @property
