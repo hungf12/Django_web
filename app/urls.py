e18a83cb52edabb4fb038b1ff2d70c558f3eca22
hungf12 <duyhung12032000@gmail.com>
2025-04-11 10:46:52 +0700

    up

--- app/urls.py
+++ app/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.home,name="home"),
     path('guarantee/', views.Guarantee,name="guarantee"),
+    path('payment_method/', views.Payment_method,name="payment_method"),
     path('transport/', views.Transport,name="transport"),
     path('stores/', views.store_list,name="store_list"),
     path('contract/', views.contract,name="contract"),
--- app/views.py
+++ app/views.py
@@ -261,3 +261,16 @@ def Guarantee(request):
         cartItems = order['get_cart_items']
     context = {'items': items,'order':order, 'cartItems': cartItems}
     return render(request, "app/guarantee.html",context)
+
+def Payment_method(request):
+    if request.user.is_authenticated:
+        customer = request.user
+        order, created = Order.objects.get_or_create(customer=customer,complete = False)
+        items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
+    else:
+        items = []
+        order = {'get_cart_items':0,'get_cart_total':0}
+        cartItems = order['get_cart_items']
+    context = {'items': items,'order':order, 'cartItems': cartItems}
+    return render(request, "app/payment_method.html",context)
